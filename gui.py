3b855a6b7982583089420091bd07c8c7bc3514ee
salidalal <34741501+salidalal@users.noreply.github.com>
2018-12-18 19:24:30 +0200

    לא

--- gui.py
+++ gui.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import urllib.request
-import urllib.parse
-import re
-from Language.Language import *
-from Article.Article import *
+from NewsClusters import NewsClusters
+from Language import Language
+from Utilities import streamNews, addToDB
+from Evaluation import Evaluation
 
 
 #creates languages and store them in an array
@@ -19,7 +18,7 @@ for i in range(len(lang)):
 pickedLang= input()
 
 #checking the lang input validation
-while pickedLang<'0' or pickedLang>'9' or int(pickedLang) > len(lang):
+while not pickedLang.isNumeric() or int(pickedLang) > len(lang):
     print(str(pickedLang)+" is invalid input try again")
     pickedLang= input()
 
@@ -37,60 +36,53 @@ print("we have "+str( numOfStoredArt )+" stored "+pickedLang.name+" articles")
 
 #option to stream news for the choosen lang
 #put streaming in a func
-
-ans = input("would you like to stream new articles? y/n").lower()
-
-while ans not in ('y','n'):
-    ans = input( (str(ans)+" is invalid input try again"))
-"""
-if ans == 'y':
+if numOfStoredArt == 0:
+    print("No stored articles --> Streaming ... ")
     pickedLang.streamNews()
-    print("now we have "+ len(pickedLang.articles)
-
-"""
+    numOfStoredArt = len(pickedLang.articles)
 
 
-numOfStoredArt = len(pickedLang.articles)
+else:    
+    ans = input("would you like to stream new articles? y/n").lower()
 
+    while ans not in ('y','n'):
+        ans = input( ans+" is invalid input try again")
+    
+    if ans == 'y':
+        pickedLang.streamNews()
+        print("now we have "+ len(pickedLang.articles))
+        numOfStoredArt = len(pickedLang.articles)
+
+if numOfStoredArt <1:
+    print("No articles to cluster \nExiting...")
+    exit
 
 numOfClusters = input("Enter num of Cluster")
+
+              
 #checking the lang input validation
-while pickedLang<'0' or pickedLang>'9' or int(pickedLang) > len(lang):
-    print(str(pickedLang)+" is invalid input try again")
-    pickedLang= input()
+while not numOfClusters.isNumeric() or int(numOfClusters) > numOfStoredArt:
+    if int(numOfClusters) > numOfStoredArt :
+        print("pick a number thats less then "+str(numOfStoredArt))
+    else:
+        print(numOfClusters+" is invalid input try again")
+    
+    numOfClusters = input()
 
 
 
-art = lang.getArticles()
+print("Starting Cluster...")
+
+articles = lang.getArticles()
+
+news = NewsClusters(articles, numOfClusters)
+news.printClusters()
+
+eval = Evaluation(news)
+eval.report()
 
 
-x = input('Enter your name:')
 
 
 
 
-
-
-
-
-
-
-for article in art:
-    print(article)
-
-#for article in art:
-    #print(article)
-
-
-#tokenized = []
-#for article in articles:                #Creating tokenized articles
- #   tokenized.append(article.split())
-
-
-#Preprocessing
-#for i in range(len(tokenized)):
- #   tokenized[i] = list(filter(lambda x: (x.isalnum()), tokenized[i])) #Removing alphanumerical words
-  #  tokenized[i] = list(map(lambda x: x.lower(), tokenized[i]))         #Shifting all words to lower-case only
-   # tokenized[i] = list(filter(lambda x: x not in stopwords, tokenized[i]))     #Removing stopwords
-
-#print(tokenized)
